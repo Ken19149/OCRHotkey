b72a1e56bee012e7cced489a1cfa15d53dc2ddcd
Ken980 <kenboy9800@gmail.com>
2024-02-04 01:59:20 +0700

    use img from clipboard
    save into img file from clipboard
    scan text from img file
    automatically delete the img file
    save the text to clipboard

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from paddleocr import PaddleOCR
+from PIL import ImageGrab
 import pyperclip
+import os
+
+img_clip = ImageGrab.grabclipboard()
+img_clip.save("OCR_temp.png", "PNG")
 
 ocr = PaddleOCR(use_angle_cls=True, lang="japan")
-img_path = "img/test.png"
+img_path = "OCR_temp.png"
 result = ocr.ocr(img_path, cls=True)
-
-# print each line
-for i in range(len(result)):
-    res = result[i]
-    for line in res:
-        print(line)
+os.remove("OCR_temp.png")
 
 text = ""
 # combine text result
 for i in range(len(result[0])):
     text += result[0][i][1][0] + "\n"
-    print(result[0][i][1][0])
 
 print(text)
 
