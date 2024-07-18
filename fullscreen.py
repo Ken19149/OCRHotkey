68ee8291f4cb385514a07cdad4c13f655eea684b
Ken980 <kenboy9800@gmail.com>
2024-07-18 21:19:57 +0700

    draw a box around scanned texts

--- fullscreen.py
+++ fullscreen.py
@@ -1,5 +1,6 @@
 from paddleocr import PaddleOCR
-from PIL import ImageGrab
+from PIL import ImageGrab, Image, ImageDraw
+import numpy as np
 import json
 import os
 
@@ -11,7 +12,20 @@ ss.close()
 result = ocr.ocr("screen_temp.png", cls=True)
 # os.remove("screen_temp.png")
 
+def drawBox(path="screen_temp.png", result=result):
+    img_box = Image.open(path) 
+    draw = ImageDraw.Draw(img_box)  
+    for i, box in enumerate(result[0]):
+        box = np.array(box[0]).astype(np.int32)
+        xmin = min(box[:, 0])
+        ymin = min(box[:, 1])
+        xmax = max(box[:, 0])
+        ymax = max(box[:, 1])
+        draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
+        draw.text((xmin, ymin), f"{i}", fill="black")
+    img_box.show()
 
+drawBox()
 
 print(result)
 
