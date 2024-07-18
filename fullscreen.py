d8e18bf1139ed041461086b9cde5c18195468b39
Ken980 <kenboy9800@gmail.com>
2024-07-18 22:08:17 +0700

    decorate screen box

--- fullscreen.py
+++ fullscreen.py
@@ -1,5 +1,5 @@
 from paddleocr import PaddleOCR
-from PIL import ImageGrab, Image, ImageDraw
+from PIL import ImageGrab, Image, ImageDraw, ImageFont
 import numpy as np
 import json
 import os
@@ -15,15 +15,18 @@ result = ocr.ocr("screen_temp.png", cls=True)
 def drawBox(path="screen_temp.png", result=result):
     img_box = Image.open(path) 
     draw = ImageDraw.Draw(img_box)  
+    font = ImageFont.load_default(size=16)
     for i, box in enumerate(result[0]):
         box = np.array(box[0]).astype(np.int32)
         xmin = min(box[:, 0])
         ymin = min(box[:, 1])
         xmax = max(box[:, 0])
         ymax = max(box[:, 1])
-        draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
-        draw.text((xmin, ymin), f"{i}", fill="black")
+        draw.rectangle((xmin, ymin, xmax, ymax), outline="#00ffff", width=4)
+        draw.text((xmin, ymin), f" {i}", fill="#ffffff", font=font, stroke_width=1)
+    img_box.save("screen_box.png")
     img_box.show()
+    img_box.close
 
 drawBox()
 
