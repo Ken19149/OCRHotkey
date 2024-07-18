9576a2e4781d497753d20441400dbb16cf74289f
Ken980 <kenboy9800@gmail.com>
2024-07-19 05:06:04 +0700

    loop through result in js and adjust font

--- v2/web/script.js
+++ v2/web/script.js
@@ -1,5 +1,8 @@
 fetch("./output/result.json").then(result => result.json()).then(result => display(result))
 
-function display(result) {
-    console.log(result)
+function display(results) {
+    for (i in results[0]) {
+        alert(results[0][i][1][0])
+    }
+    
 }
