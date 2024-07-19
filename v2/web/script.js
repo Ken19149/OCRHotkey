1c246f532488871183dc481bea4414b6ce3be1af
Ken980 <kenboy9800@gmail.com>
2024-07-19 23:16:15 +0700

    showing result on the website - position not finished

--- v2/web/script.js
+++ v2/web/script.js
@@ -1,8 +1,14 @@
 fetch("./output/result.json").then(result => result.json()).then(result => display(result))
 
 function display(results) {
+    text = ""
     for (i in results[0]) {
-        console.log(results[0][i][1][0])
+        content = results[0][i][1][0]
+        left = results[0][i][0][3][0]
+        top = results[0][i][0][3][1]
+        text += `<p style=\"position:absolute;left:${left};top:${top};\">${content}</p>`
     }
+    console.log(results)
+    document.getElementById("text").innerHTML = text
     
 }
