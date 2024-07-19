81f2bf5eda7ffa17f82695b220b8440cb21e3bd0
Ken980 <kenboy9800@gmail.com>
2024-07-19 23:39:10 +0700

    set position for text and background for website

--- v2/web/script.js
+++ v2/web/script.js
@@ -4,11 +4,10 @@ function display(results) {
     text = ""
     for (i in results[0]) {
         content = results[0][i][1][0]
-        left = results[0][i][0][3][0]
-        top = results[0][i][0][3][1]
-        text += `<p style=\"position:absolute;left:${left};top:${top};\">${content}</p>`
+        x = results[0][i][0][3][0]
+        y = results[0][i][0][0][1]
+        text += `<p style=\"position:absolute;left:${x}px;top:${y}px;\">${content}</p>`
     }
-    console.log(results)
+
     document.getElementById("text").innerHTML = text
-    
 }
